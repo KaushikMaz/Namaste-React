3d04407783bf7c2cf0c8b9a6c59a9097c0e9b8be
Kaushik <kmzdr1@gmail.com>
2023-01-19 20:53:32 +0530

    update: optimization

--- App.js
+++ App.js
@@ -1,15 +1,22 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+    import {createElement} from "react";
+    import ReactDOM from "react-dom/client";
 
-const root=document.getElementById("root")
-const heading=document.createElement("h1");
-heading.innerHTML="Namaste React"
-root.appendChild(heading)
+    const root=document.getElementById("root")
+    const heading=document.createElement("h1");
+    heading.innerHTML="Namaste React"
+    root.appendChild(heading)
 
 
+    const headingNew=createElement("h1",{},"Namaste Akshay Saini")
+    const heading2=createElement("h2",{},"Namaste Sreenu Saini")
+    const container=createElement("div",{id:"container"},[headingNew,heading2])
+    const rootNew=ReactDOM.createRoot(document.getElementById("root"));
+    rootNew.render(container)
 
-const headingNew=React.createElement("h1",{},"Namaste Akshay Saini")
-const heading2=React.createElement("h2",{},"Namaste Sreenu Saini")
-const container=React.createElement("div",{id:"container"},[headingNew,heading2])
-const rootNew=ReactDOM.createRoot(document.getElementById("root"));
-rootNew.render(container)
+
+    // JSX
+    // React Element
+   const heading3= <h1 id="title" key="h2">Namaste India</h1> 
+
+//    React component
+

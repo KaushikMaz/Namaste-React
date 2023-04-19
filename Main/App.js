27e565d76b409221fe90ec5265cbe0c516f006b2
Kaushik <kmzdr1@gmail.com>
2023-04-20 01:27:19 +0530

    update: initial setup

--- Main/App.js
+++ Main/App.js
@@ -1,3 +1,14 @@
 import React from "react";
 import ReactDOM from"react-dom/client";
 
+
+const Header=()=>{
+    return(
+        <img classsrc="/Main/Images/FoodLogo.jpg"/>
+
+    )
+}
+const Applayout=()=>{
+    
+ 
+}

ba05e389a8e4532e5414943bed696c20677a23dd
Kaushik <kmzdr1@gmail.com>
2023-05-29 23:16:20 +0530

    Component: Header component created and updated

--- Chapter-4/App.js
+++ Chapter-4/App.js
@@ -1,30 +1,5 @@
 import React from "react";
-import FoodLogo from "./Images/FoodLogo.jpg"
-const Logo=()=>{
-    return(
-        <img alt ="Food Logo" className="Logo" src={FoodLogo}/>
-
-    )
-}
-const Header=()=>{
-    return(
-        <div className="header">
-            <Logo/>
-            <div className="nav-items">
-                <ul>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Contact</li>
-                    <li>Cart</li>
-                </ul>
-
-            </div>
-            
-
-        </div>
-
-    )
-}
+import Header from "./Components/Header"
 const AppLayout=()=>{
     return(
         <Header/>
